bbe20464f6fceacaabc8731d6fc2b610149c855d
mariam <mariem.hasan@yahoo.com>
2020-05-03 15:31:05 -0700

    Final changes

--- Task1/Controllers/HomeController.cs
+++ Task1/Controllers/HomeController.cs
@@ -6,12 +6,34 @@ using System.Web.Mvc;
 
 namespace Task1.Controllers
 {
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        public string Phone { get; set; }
+        public bool WillAttend { get; set; }
+    }
     public class HomeController : Controller
     {
+        [HttpGet]
         public ViewResult Index()
         {
-            
+
             return View();
         }
+        [HttpPost]
+        public ViewResult Index(Employee employee)
+        {
+            ViewBag.Name = employee.Name;
+            if(employee != null && employee.Name != null)
+            {
+                return View("Thanks");
+            }
+            return View();
+
+           
+        }
+
+       
     }
 }
